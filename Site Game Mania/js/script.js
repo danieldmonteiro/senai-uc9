5a3f04cae1ad0defc334423dc7544f0dd399e4a6
danieldmonteiro <danieldmonteiro@gmail.com>
2022-02-15 23:24:32 -0300

    sétimo commit site

---
+++ Site Game Mania/js/script.js
@@ -0,0 +1,9 @@
+function mostrarPoppup(){
+    window.alert("oi mundo")
+}
+let email = document.getElementById("campo-email");
+
+function enviarEmail(){
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
+}
