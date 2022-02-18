9a8ff75e7402a556213edcdfa257f4a00dbdbe95
danieldmonteiro <danieldmonteiro@gmail.com>
2022-02-17 22:40:07 -0300

    oitavo commit site

--- Site Game Mania/js/script.js
+++ Site Game Mania/js/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+    $("#barras").click(function(){
+        if($("#menu").hasClass("#menu-ativo")){
+            $("menu").removeClass("#menu-ativo")
+        }else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+})
+
 function mostrarPoppup(){
     window.alert("oi mundo")
 }
