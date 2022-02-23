d1b71b823c34b151b8ef6e871fd92a0d621ffc5b
danieldmonteiro <danieldmonteiro@gmail.com>
2022-02-23 19:56:18 -0300

    decimo segundo commit site

--- Site Game Mania/js/script.js
+++ Site Game Mania/js/script.js
@@ -1,6 +1,17 @@
-function mostrarPoppup(){
-    window.alert("oi mundo")
-}
+
+// $(document).ready(function(){
+    // $("#barras").click(function(){
+        // if($("#menu").hasClass("#menu-ativo")){
+        //    $("#menu").removeClass("#menu-ativo")
+     //   }else{
+          //  $("#menu").addClass("menu-ativo")
+        // }
+    // })
+// })
+
+// function mostrarPoppup(){
+//    window.alert("oi mundo")
+//}
 let email = document.getElementById("campo-email");
 
 function enviarEmail(){
@@ -8,10 +19,10 @@ function enviarEmail(){
     console.log(emailDigitado)
 }
 
-let email = document.getElementById("campo-email-login");
-let password = document.getElementById("campo-senha");
+ let email = document.getElementById("campo-email-login");
+ let password = document.getElementById("campo-senha");
 
-function enviarEmailSenha(){
+ function enviarEmailSenha(){
     let emailDigitato = email.value;
     let passwordDigitado = password.value;
     console.log(emailDigitado + passwordDigitado)
